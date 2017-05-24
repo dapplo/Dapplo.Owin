70db1803584b72df7b86801584443680d80b22ab
Robin <robin@dapplo.net>
2017-05-24 16:06:14 +0200

    Updated dependencies. [release]

--- Dapplo.SignalR/Utils/HubActivator.cs
+++ Dapplo.SignalR/Utils/HubActivator.cs
@@ -34,41 +34,44 @@ using Microsoft.AspNet.SignalR.Hubs;
 
 namespace Dapplo.SignalR.Utils
 {
-	/// <summary>
-	///     This IHubActivator implementation uses the Dapplo.Addons to enable dependency injection
-	/// </summary>
-	[Export(typeof(IHubActivator))]
-	public class HubActivator : IHubActivator
-	{
-		[Import]
-		private IServiceLocator ServiceLocator { get; set; }
+    /// <summary>
+    ///     This IHubActivator implementation uses the Dapplo.Addons to enable dependency injection
+    /// </summary>
+    [Export(typeof(IHubActivator))]
+    public class HubActivator : IHubActivator
+    {
+        [Import]
+        private IMefServiceLocator MefServiceLocator { get; set; }
 
-		[Import]
-		private IServiceExporter ServiceExporter { get; set; }
+        [Import]
+        private IDependencyProvider DependencyProvider { get; set; }
 
-		/// <summary>
-		///     Lookup or create a IHub and inject it.
-		/// </summary>
-		/// <param name="descriptor">HubDescriptor</param>
-		/// <returns>IHub</returns>
-		public IHub Create(HubDescriptor descriptor)
-		{
-			// Have the base implementation create the hub
-			var hub = ServiceLocator.GetExport(descriptor.HubType) as IHub;
-			if (hub != null)
-			{
-				return hub;
-			}
-			hub = Activator.CreateInstance(descriptor.HubType) as IHub;
-			if (hub == null)
-			{
-				return null;
-			}
-			// Use the IServiceLocator to inject dependencies
-			ServiceLocator.FillImports(hub);
-			// Use the IServiceExporter to export the hub
-			ServiceExporter.Export(descriptor.HubType, hub);
-			return hub;
-		}
-	}
+        [Import]
+        private IServiceExporter ServiceExporter { get; set; }
+
+        /// <summary>
+        ///     Lookup or create a IHub and inject it.
+        /// </summary>
+        /// <param name="descriptor">HubDescriptor</param>
+        /// <returns>IHub</returns>
+        public IHub Create(HubDescriptor descriptor)
+        {
+            // Have the base implementation create the hub
+            var hub = MefServiceLocator.GetExport(descriptor.HubType) as IHub;
+            if (hub != null)
+            {
+                return hub;
+            }
+            hub = Activator.CreateInstance(descriptor.HubType) as IHub;
+            if (hub == null)
+            {
+                return null;
+            }
+            // Use the IServiceLocator to inject dependencies
+            DependencyProvider.ProvideDependencies(hub);
+            // Use the IServiceExporter to export the hub
+            ServiceExporter.Export(descriptor.HubType, hub);
+            return hub;
+        }
+    }
 }
